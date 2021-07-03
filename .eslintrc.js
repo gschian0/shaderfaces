d9b588c1875e208172ed11eff98f02b8ff648127
gschian0 <54407820+gschian0@users.noreply.github.com>
2021-07-03 11:14:48 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+//eslint-disable-next-line
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {},
+};
